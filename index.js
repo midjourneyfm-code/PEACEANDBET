97073754a122917c8b39134d4ed1f9d2085bf383
midjourneyfm-code <midjourneyfm@gmail.com>
2026-01-10 11:23:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -135,13 +135,13 @@ function calculatePotentialWin(amount, odds) {
 
 function getSafeOrRiskMultipliers() {
   return [
-    { round: 1, multiplier: 1.3, winChance: 65 },
-    { round: 2, multiplier: 1.6, winChance: 60 },
-    { round: 3, multiplier: 2.1, winChance: 45 },
-    { round: 4, multiplier: 3.0, winChance: 35 },
-    { round: 5, multiplier: 4.2, winChance: 30 },
-    { round: 6, multiplier: 5.8, winChance: 20 },
-    { round: 7, multiplier: 8.0, winChance: 12 },
+    { round: 1, multiplier: 1.1, winChance: 80 },
+    { round: 2, multiplier: 1.3, winChance: 70 },
+    { round: 3, multiplier: 1.8, winChance: 50 },
+    { round: 4, multiplier: 2.3, winChance: 45 },
+    { round: 5, multiplier: 3.5, winChance: 30 },
+    { round: 6, multiplier: 4.5, winChance: 25 },
+    { round: 7, multiplier: 8.1, winChance: 12 },
     { round: 8, multiplier: 12.0, winChance: 9 },
     { round: 9, multiplier: 18.0, winChance: 7 },
     { round: 10, multiplier: 30.0, winChance: 5 }
