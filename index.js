b3627c5a5f37b0d6db52b3462c4c422abf1943ce
midjourneyfm-code <midjourneyfm@gmail.com>
2026-01-11 01:05:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -152,13 +152,13 @@ function getSafeOrRiskMultipliers() {
   return [
     { round: 1, multiplier: 1.1, winChance: 65 },
     { round: 2, multiplier: 1.3, winChance: 60 },
-    { round: 3, multiplier: 1.5, winChance: 60 },
-    { round: 4, multiplier: 1.8, winChance: 60 },
-    { round: 5, multiplier: 2.1, winChance: 65 },
-    { round: 6, multiplier: 3.5, winChance: 55 },
-    { round: 7, multiplier: 4.5, winChance: 45 },
-    { round: 8, multiplier: 6.5, winChance: 35 },
-    { round: 9, multiplier: 12.5, winChance: 25 },
+    { round: 3, multiplier: 1.5, winChance: 50 },
+    { round: 4, multiplier: 1.8, winChance: 40 },
+    { round: 5, multiplier: 2.1, winChance: 30 },
+    { round: 6, multiplier: 3.5, winChance: 25 },
+    { round: 7, multiplier: 4.5, winChance: 20 },
+    { round: 8, multiplier: 6.5, winChance: 18 },
+    { round: 9, multiplier: 12.5, winChance: 15 },
     { round: 10, multiplier: 30.0, winChance: 15 }
   ];
 }
